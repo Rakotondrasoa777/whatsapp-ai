b241c68c76536d1f761c1e02a82b22067e4711e6
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2026-08-14 16:17:11 +0300

    feat: add travel price data

---
+++ src/price/priceData.js
@@ -0,0 +1,37 @@
+export const priceData = {
+    antananarivo: {
+        toamasina: {
+            lite: 45000,
+            premium: 60000,
+            vip: 85000
+        },
+
+        mahajanga: {
+            lite: 75000,
+            premium:90000,
+            vip: 125000
+        },
+
+        antsirabe: {
+            premium: 23000,
+        },
+
+        fianarantsoa: {
+            lite: 50000,
+            premium: 60000,
+        },
+
+        morondava: {
+            lite: 80000
+        },
+
+        antsiranana: {
+            lite: 155000,
+            premium: 205000,
+        },
+
+        ambatondrazaka: {
+            lite: 30000,
+        }
+    }
+};
