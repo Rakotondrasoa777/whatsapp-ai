05162fe1feb0f81feb645e681e88b657ed3035eb
Rakotondrasoa777 <rakotondrasoatojonandrianina5@gmail.com>
2026-08-14 16:20:40 +0300

    feat: update message handler

--- src/whatsapp/messageHandler.js
+++ src/whatsapp/messageHandler.js
@@ -1,28 +1,22 @@
 import { predict } from "../model_scripts/ai/index.js";
-import { processMessage } from "../message/messageBuilder.js"
+import { processMessage } from "../message/messageProcessor.js"
 import { sendMessage } from "./sender.js";
 import { getConversation, setConversation, deleteConversation } from "../conversation/conversationState.js";
-import { processConversation } from "../conversation/handleConversation.js";
+import { processConversation } from "../conversation/handleConversation.js"
 
 export async function handeMessage(message) {
     try {
         const userId = message.from
         const text = message.body
 
-        const conversation = getConversation(userId)
+        const conversation = await getConversation(userId)
 
         if (conversation) {
-            // console.log("📌 Conversation trouvée :", conversation);
 
-            const conversationResponse = processConversation(conversation, userId, text)
+            const conversationResponse = await processConversation(conversation, userId, text)
 
             if (conversationResponse) {
                 await sendMessage(userId, conversationResponse)
-
-                console.log(
-                    "🤖 Réponse conversation :",
-                    conversationResponse
-                );
             }
 
             return;
@@ -30,9 +24,6 @@ export async function handeMessage(message) {
 
         const prediction = await predict(text);
         const response = await processMessage(prediction, userId);
-
-        // console.log(response);
-        
 
         await sendMessage(userId, response);
 
